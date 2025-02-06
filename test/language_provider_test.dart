531e9ca889862efe674abeacc1f122d79d76a36a
Pepitoelgringo <kxzdani@gmail.com>
2025-02-06 17:36:06 +0100

    Añadidos test basicos de los providers

---
+++ test/language_provider_test.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:proyectocompras/languageProvider.dart';
+
+
+void main() {
+  test("El valor deberia ser español", () {
+    final languageProvider = LanguageProvider(); // CREAMOS EL OBJETO/VARIABLE QUE VAMOS A PROBAR
+    expect(languageProvider.locale, Locale("es")); // Y PONEMOS EL VALOR QUE ESPERAMOS COMO RESULTADO CORRECTO
+  });
+
+  test('Cambiar idioma a inglés', () {
+    final languageProvider = LanguageProvider();
+    languageProvider.setLocale(Locale('en'));
+    expect(languageProvider.locale, Locale('en'));
+  });
+}
---
+++ test/theme_provider_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:proyectocompras/ThemeProvider.dart';
+
+void main() {
+  test("El valor inicial debería ser modo claro", () {
+    final themeProvider = ThemeProvider();
+    expect(themeProvider.isDarkMode, false);
+  });
+
+  test("Cambiar a modo oscuro", () {
+    final themeProvider = ThemeProvider();
+    themeProvider.toggleTheme();
+    expect(themeProvider.isDarkMode, true);
+  });
+
+  test("Cambiar de nuevo a modo claro", () {
+    final themeProvider = ThemeProvider();
+    themeProvider.toggleTheme(); // Cambia a oscuro
+    themeProvider.toggleTheme(); // Cambia a claro
+    expect(themeProvider.isDarkMode, false);
+  });
+}
